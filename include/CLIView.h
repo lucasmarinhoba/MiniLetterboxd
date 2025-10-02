0dbc13b32588078eef35fed2f1382147d455622c
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2025-10-02 20:15:07 -0300

    CLIView fase 2

--- include/CLIView.h
+++ include/CLIView.h
@@ -1,14 +1,21 @@
 #pragma once
-#include "AppController.h"
+#include <vector>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <limits>
+
+// Forward declaration para evitar dependência circular
+class AppController;
 
 class CLIView {
+public:
+    CLIView(AppController& ctrl);
+    void showMainMenu();
+    void listMedias();
+    void showUserPage();
+    void runAutoDemo();
+
 private:
     AppController& controller;
-
-public:
-    CLIView(AppController& controller);
-
-    void showMainMenu();
-    void showUserPage();
-    void listMedias();
 };
