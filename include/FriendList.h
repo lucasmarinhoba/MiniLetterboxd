a6cdea87489fac02b837928284618280398fd65c
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2025-10-02 20:15:58 -0300

    Friendlist.h Fase

--- include/FriendList.h
+++ include/FriendList.h
@@ -5,9 +5,12 @@
 
 class FriendList {
 private:
-    std::vector<std::weak_ptr<User>> friends;
+    std::vector<std::weak_ptr<User>> friends; // Armazena amigos como weak_ptr para evitar ciclos
 
 public:
-    void addFriend(std::shared_ptr<User> user);
+    // Adiciona um amigo à lista
+    void addFriend(const std::shared_ptr<User>& user);
+
+    // Retorna a lista de amigos
     std::vector<std::weak_ptr<User>> getFriends() const;
 };
