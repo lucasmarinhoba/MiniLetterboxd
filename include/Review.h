19731fdc73957bfbf54a95da41fdaddea276d8f6
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2025-10-02 20:21:40 -0300

    Review.h da fase 2

--- include/Review.h
+++ include/Review.h
@@ -1,20 +1,35 @@
 #pragma once
 #include <string>
 #include <memory>
-#include "Media.h"
+
+// Forward declaration
+class Media;
+class User;
 
 class Review {
 private:
     std::shared_ptr<Media> media;
+    std::shared_ptr<User> user; // agora obrigatoriamente não nulo
     int score;
     std::string text;
-    std::string date; // "DD/MM/AAAA"
+    std::string date;
 
 public:
-    Review(std::shared_ptr<Media> media, int score, const std::string& text, const std::string& date);
+    // Construtor principal (Media + User + Score + Texto + Data)
+    Review(const std::shared_ptr<Media>& m,
+           const std::shared_ptr<User>& u,
+           int s,
+           const std::string& t,
+           const std::string& d);
 
+    // Getters
     std::string getMediaTitle() const;
     int getScore() const;
     std::string getText() const;
     std::string getDate() const;
+    std::shared_ptr<User> getUser() const;
+
+    // Setter e getter para Media
+    void setMedia(const std::shared_ptr<Media>& m);
+    std::shared_ptr<Media> getMedia() const;
 };
