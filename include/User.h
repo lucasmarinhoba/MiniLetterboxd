993ed724fb8e000804d3995cb8ef25301cec773b
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2025-10-02 20:27:08 -0300

    User.h fase 2

--- include/User.h
+++ include/User.h
@@ -11,20 +11,27 @@ private:
     std::string password;
     std::vector<std::shared_ptr<Review>> reviews;
     std::vector<std::weak_ptr<User>> friends;
+    std::vector<std::shared_ptr<Media>> watchedMedias; // Mídias assistidas
 
 public:
     User(const std::string& username, const std::string& password);
 
+    // Getters básicos
     std::string getUsername() const;
+    std::string getPassword() const; // necessário para persistência
     bool checkPassword(const std::string& pass) const;
 
+    // Amigos
     void addFriend(std::shared_ptr<User> user);
     std::vector<std::weak_ptr<User>> getFriends() const;
 
+    // Reviews
     void addReview(std::shared_ptr<Review> review);
     void removeReview(const std::string& mediaTitle);
     std::vector<std::shared_ptr<Review>> getReviews() const;
 
+    // Mídias assistidas
+    void addWatchedMedia(std::shared_ptr<Media> media);
     size_t getWatchedCount() const;
     std::string getLastReviewInfo() const;
     double getTotalWatchTimeHours() const;
